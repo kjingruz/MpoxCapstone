b1f0f966785ae819ae2135345e8d3326ff7f842d
kjingruz <58500251+kjingruz@users.noreply.github.com>
2025-04-01 21:55:16 -0400

    Update hpc_medsam2_pipeline.sh

--- MedSAM2/hpc_medsam2_pipeline.sh
+++ MedSAM2/hpc_medsam2_pipeline.sh
@@ -231,7 +231,7 @@ if [ ${DO_FINETUNE} -eq 1 ]; then
         --data_dir ${MPOX_DATA_DIR}/npy \
         --output_dir ${FINETUNE_OUTPUT_DIR} \
         --sam2_checkpoint ${SAM2_CHECKPOINT} \
-        --model_cfg "${BASE_DIR}/MedSAM2/sam2/configs/sam2.1/sam2.1_hiera_b+.yaml" \
+        --model_cfg "sam2.1/sam2.1_hiera_b+" \
         --batch_size ${BATCH_SIZE} \
         --num_epochs 30 \
         --learning_rate 1e-5 \
